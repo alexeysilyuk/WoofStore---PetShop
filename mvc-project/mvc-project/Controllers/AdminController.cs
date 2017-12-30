e5f3b6161bc5cd0dbcae196c09a5641533286f29
Server <Server@SERVER-PC>
2017-12-30 20:46:14 +0200

    admin async edit users and delete users

--- mvc-project/mvc-project/Controllers/AdminController.cs
+++ mvc-project/mvc-project/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using mvc_project.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -23,38 +24,91 @@ namespace mvc_project.Controllers
             OrderDAL odal = new OrderDAL();
             List<Order> list_of_orders = odal.Orders.ToList<Order>();
 
-            if (Convert.ToBoolean(Session["admin"]))
-            {
-                return View("Index", new AdminViewModel(list_of_users, list_of_items, list_of_orders));
-            }
+            // if (Convert.ToBoolean(Session["admin"]))
 
-            else
-            {
-                 return View("~/Views/Home/Index.cshtml");
-            }
+            return View("Index", new AdminViewModel(list_of_users, list_of_items, list_of_orders));
+
+
+            //else return View("~/Views/Home/Index.cshtml");
+
         }
 
         [HttpPost]
-        public ActionResult EditUser(User model)
+        public ActionResult EditUser(String  username, String fname, String lname, String phone, String email, String balance, String password
+            )
         {
 
             UserDAL udal = new UserDAL();
             List<User> list_of_users = udal.Users.ToList<User>();
-            var obj = list_of_users.Find(x => x.username == model.username);
+            User obj = list_of_users.Find(x => x.username == username);
+
+
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    if (obj != null)
+                    {
+                        udal.Users.Remove(obj);
+                        udal.SaveChanges();
+
+                        obj.fname = fname;
+                        obj.lname = lname;
+                        obj.phone = phone;
+                        obj.email = email;
+                        obj.money = Int32.Parse(balance);
+                        obj.password = password;
+
+                        udal.Users.Add(obj);
+                        udal.SaveChanges();
+                    }
+
+                }
+
+
+                catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+                {
+                    Exception raise = dbEx;
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            string message = string.Format("{0}:{1}",
+                                validationErrors.Entry.Entity.ToString(),
+                                validationError.ErrorMessage);
+
+                            raise = new InvalidOperationException(message, raise);
+                        }
+                    }
+                    throw raise;
+                }
+            }
+
+            return null;
+        }
+
+
+
+        [HttpPost]
+        public ActionResult DeleteUser(String username)
+        {
+
+            UserDAL udal = new UserDAL();
+            List<User> list_of_users = udal.Users.ToList<User>();
+            var obj = list_of_users.Find(x => x.username == username);
 
 
             if (obj != null)
             {
-                udal.Entry(obj).CurrentValues.SetValues(model);
+                udal.Users.Remove(obj);
                 udal.SaveChanges();
             }
-
-
-
 
 
             return null;
         }
 
-    }
+    }   
 }
--- mvc-project/mvc-project/Scripts/main.js
+++ mvc-project/mvc-project/Scripts/main.js
@@ -1,28 +1,40 @@
 ﻿$(function () {
+    var phone, email, lname, fname, balance, pass, username, photo_url;
 
+
+    $(".del_btn").click(function () {
+        var idUser = $(this).attr("id");
+        var r = confirm("Are you really want to delete user: " + idUser);
+        if (r == true) {
+            $.post("/Admin/DeleteUser", { username: idUser });
+            $(this).parent().parent().fadeOut();
+
+        } else {
+            console.log("Cancel!");
+        }
+    });
 
     $(".edit_btn").click(function () {
         $(".shadow, .userModal").fadeIn();
 
-        var phone = $(this).parent().prev().prev().text();
-        var email = $(this).parent().prev().prev().prev().text();
-        var lname = $(this).parent().prev().prev().prev().prev().text();
-        var fname = $(this).parent().prev().prev().prev().prev().prev().text();
-        var balance = $(this).parent().prev().prev().prev().prev().prev().prev().text();
-        var pass = $(this).parent().prev().prev().prev().prev().prev().prev().prev().text();
-        var username = $(this).parent().prev().prev().prev().prev().prev().prev().prev().prev().text();
+         phone = $(this).parent().prev().prev();
+         email = $(this).parent().prev().prev().prev();
+         lname = $(this).parent().prev().prev().prev().prev();
+         fname = $(this).parent().prev().prev().prev().prev().prev();
+         balance = $(this).parent().prev().prev().prev().prev().prev().prev();
+         pass = $(this).parent().prev().prev().prev().prev().prev().prev().prev();
+         username = $(this).parent().prev().prev().prev().prev().prev().prev().prev().prev();
+         photo_url = $(this).parent().prev().prev().prev().prev().prev().prev().prev().prev().prev();
 
-        var photo_url = $(this).parent().prev().prev().prev().prev().prev().prev().prev().prev().prev().text();
 
-
-        $("#phone").val(phone);
-        $("#email").val(email);
-        $("#lname").val(lname);
-        $("#fname").val(fname);
-        $("#balance").val(balance);
-        $("#password").val(pass);
-        $("#username").val(username);
-        $("#photo_url").val(photo_url);
+        $("#phone").val(phone.text());
+        $("#email").val(email.text());
+        $("#lname").val(lname.text());
+        $("#fname").val(fname.text());
+        $("#balance").val(balance.text());
+        $("#password").val(pass.text());
+        $("#username").val(username.text());
+        $("#photo_url").val(photo_url.text());
 
     });
 
@@ -33,21 +45,30 @@
 
 
     $(".save_changes").click(function () {
-        $.post("Admin/EditUser",
+        $.post("/Admin/EditUser",
             {
+                username: $("#username").val(),
                 fname: $("#fname").val(),
                 lname: $("#lname").val(),
-                balance: $("#money").val(),
-                photo: "",
-                isAdmin: false,
+                phone: $("#phone").val(),
                 email: $("#email").val(),
-                username: $("#username").val(),
+                balance: $("#balance").val(),
                 password: $("#password").val()
-                
-            }, function (data) {
-                console.log("send");
-            console.log(data);
-        });
+               
+            });
+
+        $("#res_query").text("Successfuly changed");
+
+        fname.text($("#fname").val());
+        lname.text($("#lname").val());
+        phone.text($("#phone").val());
+        balance.text($("#balance").val());
+        email.text($("#email").val());
+        pass.text($("#password").val());
+
+
+
+        setTimeout(function () { $(".shadow, .userModal").fadeOut(); }, 2000);
     });
 
 
